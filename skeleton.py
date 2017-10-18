e87fc029f9a22052db9e12e746303c7fc3aeb6bb
Jacob <jaykoknight@btinternet.com>
2017-10-18 11:35:13 +0100

    Added NativeDroid2 files and begun working on UI

--- skeleton.py
+++ skeleton.py
@@ -1,4 +1,10 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    test = input("Local (l) or LAN (t)")
+    if(test == "l"):
+        app.run(debug=True, use_reloader=False)
+    elif(test == "t"):
+        app.run(host="0.0.0.0", debug=True, use_reloader=False)
+    else:
+        print("Wrong input")
